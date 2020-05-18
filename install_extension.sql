1515e5b0214fd712dbb390238944f3ba957d6a11
fengkx <liangkx8237@gmail.com>
2020-05-18 16:55:14 +0800

    fix: remove extra begin

--- install_extension.sql
+++ install_extension.sql
@@ -4,4 +4,4 @@ CREATE TEXT SEARCH CONFIGURATION chinese (PARSER = zhparser);
 ALTER TEXT SEARCH CONFIGURATION chinese ADD MAPPING FOR n,v,a,i,e,l WITH simple;
 
 -- Install jieba
-BEGIN CREATE EXTENSION pg_jieba;
+CREATE EXTENSION pg_jieba;
